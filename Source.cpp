446b14f7118c8ba2eb3277aa7045ed40906301ed
Andrey Levickiy <61742550+AndreyLevizkij@users.noreply.github.com>
2020-12-14 21:29:01 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,172 @@
+#include<iostream>
+#include<Windows.h>
+#include <algorithm> 
+#include <queue> 
+
+using namespace std;
+
+struct process {
+	int num;
+	int priority = 0;
+	int arrival_time = 0;
+	int burst_time = 0;
+	int total_time = 0;
+	bool is_accepted = false;
+};
+
+bool compare1(process p1, process p2)
+{
+	return p1.arrival_time < p2.arrival_time;
+}
+
+void main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int n;
+	int tq;
+	int a;
+	int b;
+
+	do {
+		cout << "Введите количество процессов: ";
+		cin >> n;
+	} while (n < 1);
+	do {
+		cout << "Введите размер кванта: ";
+		cin >> tq;
+	} while (tq < 1);
+	do {
+		cout << "Введите скорость повышения приоритета нового процесса: ";
+		cin >> a;
+	} while (a < 0);
+	do {
+		cout << "Введите скорость повышения приоритета выбранного процесса: ";
+		cin >> b;
+	} while (b < 0);
+
+	process* p = new process[n];
+	int* burst_remaining = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cout << "Введите время появления процесса " << i + 1 << ": ";
+		cin >> p[i].arrival_time;
+		cout << "Введите время выполнения процесса " << i + 1 << ": ";
+		cin >> p[i].burst_time;
+		burst_remaining[i] = p[i].burst_time;
+		p[i].num = i + 1;
+		cout << endl;
+	}
+
+	sort(p, p + n, compare1);
+
+	int m = 0;
+	for (int i = 0; i < n; i++) {
+		m += p[i].burst_time;
+	}
+	int* make = new int[m];
+	m = 0;
+
+	queue<int> q;
+	q.push(0);
+	int current_time = 0; // Текущее время
+	int completed = 0; // Количество выполненных процессов
+	int max_priority = 0; // Максимальный приоритет
+	int min_priority_accepted = 0; // Минимальный приоритет
+	int idx = -1;
+
+	if (a != 0) { 
+		if (b == 0 || b / a >= 1) { p[0].is_accepted = true; }
+	}
+
+	while (completed != n) {
+
+		if (!q.empty()) { idx = q.front(); q.pop();}
+		
+		max_priority = 0;
+		min_priority_accepted = 0;
+
+		bool flag = false;
+		if (idx >= 0) {
+			if (p[idx].arrival_time <= current_time) {
+				if (burst_remaining[idx] - tq > 0) {
+					make[m] = p[idx].num;
+					m++;
+					burst_remaining[idx] -= tq;
+					current_time += tq;
+					flag = true;
+				}
+				else {
+					if (idx == 0 && p[idx].arrival_time == 0) { p[idx].total_time += tq; }
+					current_time += burst_remaining[idx];
+					make[m] = p[idx].num;
+					m++;
+					burst_remaining[idx] = 0;
+					completed++;
+					flag = true;
+				}
+			}
+		}
+		if (flag == false) { current_time++; }
+
+		for (int i = 0; i < n; i++) {
+			if (burst_remaining[i] > 0 && p[i].arrival_time <= current_time) {
+				p[i].total_time += tq;
+				if (p[i].is_accepted == false) { p[i].priority += a; }
+				if (p[i].is_accepted == true) { p[i].priority += b; }
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (burst_remaining[i] > 0 && p[i].arrival_time <= current_time) {
+				if (p[i].priority > max_priority) { 
+					max_priority = p[i].priority; 
+					if (p[i].is_accepted == true) {
+						min_priority_accepted = max_priority;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (burst_remaining[i] > 0 && p[i].arrival_time <= current_time && p[i].is_accepted == true) {
+				if (p[i].priority < min_priority_accepted) { min_priority_accepted = p[i].priority; }
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (burst_remaining[i] > 0 && p[i].arrival_time <= current_time) {
+				if (p[i].priority >= min_priority_accepted && p[i].is_accepted == false) {
+					p[i].is_accepted = true; cout << p[i].priority;
+					while (!q.empty()){ q.pop(); }
+				}
+			}
+		}		
+
+		int k = 0;
+		int ik = -1;
+		idx = -1;
+		for (int i = 0; i < n; i++) {
+			if (burst_remaining[i] > 0 && p[i].arrival_time <= current_time && p[i].priority >= max_priority && p[i].is_accepted == true) {
+				k++;
+				ik = i;
+			}
+		}
+		if (k == 1) { idx = ik; }
+		else if (q.empty() && k > 1) {
+			for (int i = 0; i < n; i++) {
+				if (burst_remaining[i] > 0 && p[i].arrival_time <= current_time && p[i].priority >= max_priority && p[i].is_accepted == true) {
+					q.push(i);
+				}
+			}
+		}
+	}
+	for (int i = 0; i < m; i++) {
+		cout << make[i] << " "; 
+	}
+	cout << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "Общее время в системе процесса " << i + 1 << ": " << p[i].total_time << endl;
+	}
+}
